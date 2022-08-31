eb6d7fd1c5b395eb3d75817ac6129e22c12499fd
Cyboticx LLC <opokudavid446@gmail.com>
2022-08-31 02:31:24 +0000

    removed keys

--- dist/integrations/mongooose.js
+++ dist/integrations/mongooose.js
@@ -5,8 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const config_1 = __importDefault(require("../config"));
-console.log("DB user: ", config_1.default.DB_USER);
-console.log("DB password: ", config_1.default.DB_PASSWORD);
 const db = `mongodb+srv://${config_1.default.DB_USER}:${config_1.default.DB_PASSWORD}@cluster0.p87cqe2.mongodb.net/?retryWrites=true&w=majority`;
 mongoose_1.default.connect(db);
 exports.default = mongoose_1.default;
--- src/integrations/mongooose.ts
+++ src/integrations/mongooose.ts
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 import config from "../config";
 
-console.log("DB user: ", config.DB_USER);
-console.log("DB password: ", config.DB_PASSWORD);
 // const localDBUri = `mongodb://${config.DB_HOST}:${config.DB_PORT}/ecommerce`;
 const db = `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.p87cqe2.mongodb.net/?retryWrites=true&w=majority`;
 
